8518b184f605e91a41eaa8998975898d616cbf6c
anujfeb14 <anujprajapati1402@gmail.com>
2024-01-08 14:17:52 +0530

    Chapter 3

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+
+const HeadingComponent = () =>{
+    return <h1>Heading</h1>
+}
+
+const MainComponent = () => (
+    <div id='main'>
+        {HeadingComponent()}
+        <HeadingComponent></HeadingComponent>
+        <HeadingComponent/>
+        <div className='child'>
+            <h2>This is react functional component.</h2>
+        </div>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<MainComponent/>)
